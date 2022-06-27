88ae1f8e372132acc07d4c74d6d7bf05e1fc5357
nohandsome <1797695463@qq.com>
2022-06-27 21:49:26 +0800

    add logout and info analyse

--- code/client/client.py
+++ code/client/client.py
@@ -5,35 +5,44 @@ import sys
 from json import dumps
 from json import loads
 import deal_msg_func
+import pyttsx3
 
+username = ""
 
 def on_open(ws):
 
     # login
+    global username
     login_msg = deal_msg_func.gen_login_msg()
+    tmp = loads(login_msg)
+    username = tmp["content"]["name"]
+
     ws.send(login_msg)
 
     # 对于单个用户采用多线程的方式来实现输出输入的分隔
     def run(*args):
         while True:
             sendmsg = input()
+            sendmsg_json = deal_msg_func.analyse_input(sendmsg, username)
             try:
-                ws.send(sendmsg)
+                ws.send(sendmsg_json)
+                if loads(sendmsg_json)["type"] == "logout":
+                    ws.close()
+                    os._exit(0)
             except:
                 print('connection is already closed.')
                 sys.exit(0)
 
     threading.Thread(target=run).start()
 
-
+'''
 def on_error(ws, error): 
     # 对于error发生进行处理
     print("something bad happened!")
     print("error msg:{}".format(error))
-
+'''
 
 def on_close(ws, close_status_code, close_msg):
-    # 客户端退出
     if close_status_code or close_msg:
         print('close status code:', close_status_code)
         print('close msg:', close_msg)
@@ -47,13 +56,25 @@ def on_close(ws, close_status_code, close_msg):
 def on_message(ws, json_message):
     msg = loads(json_message)
     if msg["type"] == "shutdown":
-        deal_msg_func.deal_shutdown_msg(ws)
+        print("wrong username or password!")
+        # 客户端退出
+        logoutdict = {
+            "type": "logout",
+            "content": {
+                "name": username
+            }
+        }
+        logoutjson = dumps(logoutdict)
+        ws.send(logoutjson)
+        ws.close()
+        os._exit(0)
     elif msg["type"] == "pass":
-        print("connect success!")
+        pass
+
 
 
 ws_app = websocket.WebSocketApp("ws://localhost:8765",
                                 on_open=on_open, on_message=on_message,
-                                on_close=on_close, on_error=on_error)
+                                on_close=on_close)
 
 ws_app.run_forever()
--- code/client/deal_msg_func.py
+++ code/client/deal_msg_func.py
@@ -1,5 +1,13 @@
 from json import dumps
 from json import loads
+from telnetlib import LOGOUT
+
+# 中文分词
+import jieba
+import jieba.posseg as pseg #词性标注
+import jieba.analyse as anls
+from sqlalchemy import false #关键词提取
+
 
 
 ##########
@@ -18,10 +26,110 @@ def gen_login_msg():
     json_login_msg = dumps(tmpdict)
     return json_login_msg
 
-
+'''
 #############
 #shutdownmsg#
 #############
 def deal_shutdown_msg(ws):
     print("wrong username or password!")
     ws.close()
+'''
+
+############
+#deal input#
+############
+def analyse_input(input_msg, username):
+    input_msg_cut = jieba.lcut(input_msg, cut_all=false)
+    print(input_msg_cut)
+    if input_msg_cut[0] == "查询":
+        # 查询天气或者是疫情
+        if "疫情" in input_msg_cut:
+            send_msg_dict = {
+                "type": "cmd",
+                "content": {
+                    "cmd_type": "epidemic",
+                    "text": input_msg_cut[1]
+                }
+            }
+            send_msg_json = dumps(send_msg_dict)
+            return send_msg_json
+
+        elif "天气" in input_msg_cut:
+            send_msg_dict = {
+                "type": "cmd",
+                "content": {
+                    "cmd_type": "weather",
+                    "text": input_msg_cut[1]
+                }
+            }
+            send_msg_json = dumps(send_msg_dict)
+            return send_msg_json
+    
+    elif input_msg_cut[0] == "退出":
+        send_msg_dict = {
+            "type": "logout",
+            "content": {
+                "name": username
+            }
+        }
+        send_msg_json = dumps(send_msg_dict)
+        return send_msg_json
+
+    elif input_msg_cut[0] == "和":
+        target_name = input_msg_cut[1]
+        text = ""
+        for i in range(3, len(input_msg_cut)):
+            text += input_msg_cut[i]
+        send_msg_dict = {
+            "type": "cmd",
+            "content": {
+                "cmd_type": "send_msg",
+                "text": text,
+                "target_name": target_name
+            }
+        }
+        send_msg_json = dumps(send_msg_dict)
+        return send_msg_json
+
+    elif input_msg_cut[0] == "闲聊":
+        tmp_str = ""
+        for i in range(2, len(input_msg_cut)):
+            tmp_str += input_msg_cut[i]
+        send_msg_dict = {
+            "type": "cmd",
+            "content": {
+                "cmd_type": "chat",
+                "text": tmp_str 
+            }
+        }
+        send_msg_json = dumps(send_msg_dict)
+        return send_msg_json
+
+    elif "故事" in input_msg_cut:
+        send_msg_dict = {
+            "type": "cmd",
+            "content": {
+                "cmd_type": "story"
+            }
+        }
+        send_msg_json = dumps(send_msg_dict)
+        return send_msg_json
+
+    elif "笑话" in input_msg_cut:
+        send_msg_dict = {
+            "type": "cmd",
+            "content": {
+                "cmd_type": "joke"
+            }
+        }
+        send_msg_json = dumps(send_msg_dict)
+        return send_msg_json
+
+    else:
+        send_msg_dict = {
+            "type": "pass"
+        }
+        send_msg_json = dumps(send_msg_dict)
+        return send_msg_json
+
+
